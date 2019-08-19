e067688d21f1bd26bf8a2c2da6d38ede392b3d58
stefenmenzel <stefenmenzel@gmail.com>
2019-08-18 19:17:10 -0500

    got some of the layout/styling going on the main banner section

---
+++ src/components/MainBanner/MainBanner.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import {Grid} from '@material-ui/core'
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from "gatsby-image"
+
+import forbes from '../../images/forbes_white.png';
+
+import styles from './MainBanner.module.css'
+
+class MainBanner extends Component{
+    
+    render(){
+        return(
+            <div className={styles.mainBanner}>
+                <Grid container justify="center" alignItems="center">
+                        <Grid item xs={12} sm={12} className={styles.accolades}>
+                            <img
+                                src={forbes}
+                                alt="accolade1"
+                                className={styles.imgFluid}
+                            />
+                            <img
+                                src={forbes}
+                                alt="accolade2"
+                                className={styles.imgFluid}
+                            />                            
+                        </Grid>                    
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default MainBanner
