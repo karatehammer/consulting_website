a3331da0b0483acf66cd95120fdd53188f9020be
stefenmenzel <stefenmenzel@gmail.com>
2019-08-19 16:25:16 -0500

    Got the Banner part of the website looking good. Moving on to the next section

--- src/components/MainBanner/MainBanner.jsx
+++ src/components/MainBanner/MainBanner.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Grid} from '@material-ui/core'
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from "gatsby-image"
+import {Link} from "gatsby"
 
 import forbes from '../../images/forbes_white.png';
 
@@ -26,7 +27,22 @@ class MainBanner extends Component{
                             />                            
                         </Grid>
                         <Grid item xs={12} sm={12}>
-                            <p className={styles.bannerText}>This is our motto!</p>
+                            <h2 className={styles.bannerHeader}>When all you have is a hammer, all code challenges are nails.</h2>
+                        </Grid>
+                        <Grid item xs={12} sm={12}>
+                            <p className={styles.bannerText}>
+                                Our expert developers make all the best code type stuff. We are definitely
+                                the best you've ever worked with. Several definitely credible publications have listed
+                                us the best of the best. We have a proven track record of making all the best stuff.
+                                When you work with HammerSoft, it's hammertime!
+                            </p>                            
+                        </Grid>
+                        <Grid item xs={12} sm={12}>
+                            <div className={styles.linkContainer}>
+                                <Link className={styles.linkButton}>A Page</Link>
+                                <Link className={styles.linkButton}>Another Page</Link>
+                                <Link className={styles.linkButton}>A Third Page</Link>
+                            </div>                            
                         </Grid>
                 </Grid>
             </div>
