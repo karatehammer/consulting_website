bb5faa5587435765795d82bc3fbbcc37f4fb17bc
stefenmenzel <stefenmenzel@gmail.com>
2019-08-28 11:58:52 -0500

    got some icons for companies worked with, styled them a bit

--- src/components/Collaborations/Collaborations.jsx
+++ src/components/Collaborations/Collaborations.jsx
@@ -3,6 +3,11 @@ import {Grid} from '@material-ui/core'
 
 import styles from './Collaborations.module.css'
 
+import ThomsonReuters from '../../images/ThomsonReuters_400x400.jpg';
+import LeadPages from '../../images/leadpages-icon.jpg';
+import BrightPeak from '../../images/brightpeak-icon.png';
+import BIWorldwide from '../../images/BI-Worldwide_400x400.jpg';
+
 class Collaborations extends Component{
 
     render(){
@@ -12,14 +17,47 @@ class Collaborations extends Component{
                     <Grid item xs={10} sm={10} md={10}>
 
                         <Grid item xs={12} sm={12} md={12}>
-                            <h1 className={styles.collabHeader}>Here be the collaborations</h1>
+                            <h1 className={styles.collabHeader}>We've proudly worked for the following clients</h1>
                         </Grid>
                         <Grid item xs={12} sm={12} md={12}>
                             <p className={styles.collabDescription}>
-                                Here are some of the companies we've worked with in the past.
+                                We'd like to add you to this list.
                             </p>
                         </Grid>
+                        
+                        <Grid item xs={12} sm={12} md={12}>
+                            <Grid item xs={12} sm={3} md={3} className={styles.imgContainer}>
+                                <img
+                                    src={ThomsonReuters}
+                                    alt="Thomson Reuters"
+                                    className={styles.imgFluid}
+                                />
+                            </Grid>                             
 
+                            <Grid item xs={12} sm={3} md={3} className={styles.imgContainer}>
+                                <img
+                                    src={LeadPages}
+                                    alt="Lead Pages"
+                                    className={styles.imgFluid}
+                                />
+                            </Grid>
+
+                            <Grid item xs={12} sm={3} md={3} className={styles.imgContainer}>
+                                <img
+                                    src={BrightPeak}
+                                    alt="Bright Peak Financial"
+                                    className={styles.imgFluid}
+                                />
+                            </Grid>
+
+                            <Grid item xs={12} sm={3} md={3} className={styles.imgContainer}>
+                                <img
+                                    src={BIWorldwide}
+                                    alt="BI Worldwide"
+                                    className={styles.imgFluid}
+                                />
+                            </Grid>
+                        </Grid>
                     </Grid>                    
                 </Grid>                
             </div>
