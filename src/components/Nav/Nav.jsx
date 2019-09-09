350e2c110778408667a259f1ab055edd121b7a13
stefenmenzel <stefenmenzel@gmail.com>
2019-09-09 17:15:20 -0500

    got a bunch of stuff with the nav done. It still needs some styling but it's coming along, looking a lot better

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,36 +1,75 @@
 import {Link} from 'gatsby';
 import React, {Component} from 'react';
-import {Grid, List, ListItem, Typography, IconButton} from '@material-ui/core';
+import {Grid, List, ListItem, Typography, Menu, MenuItem, IconButton} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import styles from './Nav.module.css';
 
 class Nav extends Component{
+
+    state = {
+        anchorEl : null
+    }
     
     smallScreen = window.innerWidth <= 850;
 
-    toggleMenu = () => {
-        console.log('doing a thing');
+    toggleMenu = (event) => {
+        this.setState({
+            ...this.state,
+            anchorEl: event.currentTarget
+        })
+    }
+
+    handleClose = () => {
+        this.setState({
+            ...this.state,
+            anchorEl: null
+        })
     }
 
     conditionalNav = () => {
         if(this.smallScreen){
             console.log('screen size: ', window.innerWidth);
            return (
-               <IconButton aria-label="Menu" onClick={this.toggleMenu}>
-                   <MenuIcon />
-                   {/* <List>
-                       <ListItem component={Link} to="/about" button>
-                           <MenuIcon />
-                       </ListItem>
-                       <ListItem component={Link} to="/about" button>
-                           <Typography variant="p" className={styles.headerLinks}>About</Typography>
-                       </ListItem>
-                       <ListItem component={Link} to="/about" button>
-                           <Typography variant="p" className={styles.headerLinks}>Another</Typography>
-                       </ListItem>
-                   </List> */}
-               </IconButton>                
+               <>
+                   <IconButton alignItems="flex-end" className={styles.menuButton} aria-label="Menu" onClick={this.toggleMenu}>
+                    <MenuIcon />
+                    {/* <List>
+                        <ListItem component={Link} to="/about" button>
+                            <MenuIcon />
+                        </ListItem>
+                        <ListItem component={Link} to="/about" button>
+                            <Typography variant="p" className={styles.headerLinks}>About</Typography>
+                        </ListItem>
+                        <ListItem component={Link} to="/about" button>
+                            <Typography variant="p" className={styles.headerLinks}>Another</Typography>
+                        </ListItem>
+                    </List> */}
+                </IconButton>
+                <Menu
+                    anchorEl={this.state.anchorEl}
+                    keepMounted
+                    open={Boolean(this.state.anchorEl)}
+                    onClose={this.handleClose}
+                >
+                    <MenuItem onClick={this.handleClose}>
+                            <Link
+                            to="/about"
+                            className={styles.headerLinks}
+                            >
+                            About
+                            </Link>
+                    </MenuItem>
+                    <MenuItem onClick={this.handleClose}>
+                            <Link
+                            to="/about"
+                            className={styles.headerLinks}
+                            >
+                            Another
+                            </Link>  
+                    </MenuItem>
+                </Menu>
+               </>
             )
         }
         else{
